06aabf87d71b12c87308c22ffa205a6052b2b7ab
Ashley Rushworth <rushworth.ashley@gmail.com>
2020-03-02 21:21:29 +0000

    Update first query based on assignment clarification

--- src/RDFProcessing.java
+++ src/RDFProcessing.java
@@ -30,20 +30,20 @@ public class RDFProcessing {
                 ResultSetFormatter.out(results);
             }
             
-            // Find the name(s) of the most recent Nobel Chemistry award winner(s)
+            // Find the name of one of the most recent Nobel Chemistry award winners
             try (QueryExecution query = QueryExecutionFactory.create(
                     "PREFIX category: <http://data.nobelprize.org/resource/category/> "
                     + "PREFIX foaf: <http://xmlns.com/foaf/0.1/> "
                     + "PREFIX nobel: <http://data.nobelprize.org/terms/> "
-                    + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
                     + "SELECT ?name "
                     + "WHERE { "
-                        + "?nobel rdf:type nobel:NobelPrize ; "
-                        + "nobel:category category:Chemistry ; "
-                        + "nobel:year 2016 ; "
+                        + "?nobel nobel:category category:Chemistry ; "
+                        + "nobel:year ?year ; "
                         + "nobel:laureate ?laureate . "
                         + "?laureate foaf:name ?name . "
-                    + "}"
+                    + "} "
+                    + "ORDER BY DESC(?year) "
+                    + "LIMIT 1"
                     , dataset)) {
                 ResultSet results = query.execSelect();
                 ResultSetFormatter.out(results);
@@ -57,7 +57,7 @@ public class RDFProcessing {
                     + "WHERE { "
                         + "?nobel rdf:type nobel:NobelPrize ; "
                         + "nobel:category ?category ; "
-                        + "nobel:year 1902 . "
+                        + "nobel:year 1901 . "
                     + "}"
                     , dataset)) {
                 ResultSet results = query.execSelect();
